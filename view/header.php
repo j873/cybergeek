66596c374407639e5632dc090f4798f9a9d1d59f
j873 <jd9730541@gmail>
2023-10-18 21:00:48 -0300

    "PHP pqp"

--- view/header.php
+++ view/header.php
@@ -1,6 +1,6 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/e-commerce/index.php">Cybergeek </a>
+        <a class="navbar-brand" href="index.php">Cybergeek </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -17,7 +17,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="cadastro.php">
+                    <a class="nav-link" href="cadastrar.php">
                         <i class="fas fa-user"></i> Cadastrar
                     </a>
                 </li>
